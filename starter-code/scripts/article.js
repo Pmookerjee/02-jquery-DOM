efcf6f3cbf7563f45984318c667111b4a992d88b
Pmookerjee <pmookerjee@gmail.com>
2017-03-30 22:44:14 -0700

    applied auto-indent

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -31,26 +31,26 @@ Article.prototype.toHtml = function() {
   /* TODO: Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
   We need to fill in:
-    1. author name,
-    2. author url,
-    3. article title,
-    4. article body, and
-    5. publication date. */
-      $newArticle.find('address a').html(this.author);
-      $newArticle.find('address a').attr('href', this.authorUrl);
-      $newArticle.find('h1').html(this.title);
-      $newArticle.find('time').html(this.publishedOn);
-      $newArticle.find('.article-body').html(this.body);
+  1. author name,
+  2. author url,
+  3. article title,
+  4. article body, and
+  5. publication date. */
+  $newArticle.find('address a').html(this.author);
+  $newArticle.find('address a').attr('href', this.authorUrl);
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('time').html(this.publishedOn);
+  $newArticle.find('.article-body').html(this.body);
 
 
 
-      // $newArticle.authorUrl.text = this.authorUrl;
-      // $newArticle.title = this.title;
-      // $newArticle.body = this.body;
-      // $newArticle.publishedOn = this.publishedOn;
-    // lots of $newArticle.find...  (look at jQuery $.find docs)
+  // $newArticle.authorUrl.text = this.authorUrl;
+  // $newArticle.title = this.title;
+  // $newArticle.body = this.body;
+  // $newArticle.publishedOn = this.publishedOn;
+  // lots of $newArticle.find...  (look at jQuery $.find docs)
 
-    console.log($newArticle);
+  console.log($newArticle);
 
   // Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
